fe3caac53ab7dbe16a66026dd39cdfcc3fd03966
Arturo Porthe <arturo.porthe@eynes.com.ar>
2021-12-12 11:26:02 -0300

    added validations for addAnswer and addPoll

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+import re
+
 from flask import Flask, request, json, Response
-from flask import Flask, request, json, Response
+from flask import Flask, request, json, Response, abort
 from pymongo import MongoClient
 import datetime
 from bson import ObjectId
@@ -8,18 +10,19 @@ print("connecting database...")
 client = MongoClient(host='db')
 print("connection established with Mongo")
 
+
 class MongoAPI:
     def __init__(self, document):
         cursor = client["polls"]
         self.collection = cursor[document]
 
     def read(self):
-        documents =  self.collection.find()
+        documents = self.collection.find()
         output = [{item: str(data[item]) for item in data} for data in documents]
         return output
 
-    def find_byid(self,id):
-        return self.collection.find_one({"_id" : ObjectId(id)})
+    def find_byid(self, id):
+        return self.collection.find_one({"_id": ObjectId(id)})
 
     def write(self, data):
         new_document = data
@@ -28,23 +31,46 @@ class MongoAPI:
         return str(result.inserted_id)
 
 
-app = Flask(__name__)   
+app = Flask(__name__)
+
 
 @app.route('/')
 def base():
     return "status:up"
 
+
 @app.route('/addAnswer')
 def answers_post():
     data = request.json
-    res = MongoAPI("answers").write(data)
-    return Response(response=json.dumps(res),status=200,mimetype='application/json')
+
+    if 'poll_id' in data and 'answer' in data:
+        if data['answer'] is not '':
+            polls = MongoAPI("polls").read()
+            polls_ids = [poll['_id'] for poll in polls]
+            if data['poll_id'] not in polls_ids:
+                return abort(400, description='Error: There is no poll with id {}'.format(data['poll_id']))
+            res = MongoAPI("answers").write(data)
+            return Response(response=json.dumps(res), status=200, mimetype='application/json')
+        else:
+            return abort(400, description='Error: Answer cannot be left blank')
+    else:
+        return abort(400, description='Error: Answer has an incorrect format')
+
 
 @app.route('/addPoll')
 def polls_post():
     data = request.json
-    res = MongoAPI("polls").write(data)
-    return Response(response=json.dumps(res),status=200,mimetype='application/json')    
+    if 'question' in data:
+        pattern = '\\¿(.*?)\\?'
+        substring = re.search(pattern, data['question'])
+        if substring is not None:
+            res = MongoAPI("polls").write(data)
+            return Response(response=json.dumps(res), status=200, mimetype='application/json')
+        else:
+            return abort(400, description='Error: Question has an incorrect format')
+    else:
+        return abort(400, description='Error: Question has an incorrect format')
+
 
 @app.route('/getPolls')
 def polls_get():
@@ -58,7 +84,3 @@ def polls_get():
 if __name__ == "__main__":
     print('up')
     app.run(host='0.0.0.0', debug=True)
-
-
-
-
